56359b49d8f39f9300e663aace189255fd0e8e7a
bag131 <bag131@txstate.edu>
2018-07-15 17:03:16 -0500

    initial commit, working mobile vision by Google.  Need to look in to offline support.

---
+++ app/src/main/java/com/example/jungl/one_click_phone_call/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.jungl.one_click_phone_call;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView i1;
+    Button b1;
+    TextView t1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        i1 = (ImageView)findViewById(R.id.the_image);
+        b1 = (Button)findViewById(R.id.one_click_button);
+        t1 = (TextView) findViewById(R.id.phone_text);
+
+        b1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                get_text_from_image(v);
+            }
+        });
+    }
+
+    public void get_text_from_image(View v){
+
+       Bitmap bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.one);
+       TextRecognizer TR = new TextRecognizer.Builder(getApplicationContext()).build();
+
+       if(!TR.isOperational()){
+           Toast.makeText(getApplicationContext(),"could not open text recognizer.", Toast.LENGTH_SHORT).show();
+       }
+       else{
+           Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+           SparseArray<TextBlock> items = TR.detect(frame);
+
+           StringBuilder sb = new StringBuilder();
+
+           for (int i =0; i < items.size(); i++){
+               TextBlock tb = items.valueAt(i);
+               sb.append(tb.getValue());
+           }
+
+           t1.setText(sb.toString());
+       }
+    }
+}
