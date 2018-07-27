3d63d24404514264675bc81deb0746c559b092de
bag131 <bag131@txstate.edu>
2018-07-26 23:58:50 -0500

    finished the app.  Could not implement what Marcos had wanted completely since if reduces the reliability of the app to not have it continuously update the user.  Added javadoc, made UML, added the SimpleUml Jar file, PlantUML plugin and etc.

--- app/src/main/java/com/example/jungl/one_click_phone_call/MainActivity.java
+++ app/src/main/java/com/example/jungl/one_click_phone_call/MainActivity.java
@@ -5,14 +5,10 @@ package com.example.jungl.one_click_phone_call;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
@@ -20,17 +16,14 @@ import android.content.Intent;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -40,6 +33,10 @@ import com.google.android.gms.vision.text.TextRecognizer;
 
 import java.io.ByteArrayOutputStream;
 
+/**
+ * @author Benjamin Garrard, Marcos Nino, Quinton Thompson
+ * @version 1.0
+ */
 public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback, Camera.PreviewCallback{
 
 
@@ -49,36 +46,64 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
 
     SurfaceHolder surfaceHolder;
     Camera cam;
-    Canvas canvas;
-    Paint paint;
     View rectangleView;
     TextView phoneNumberText;
+    Bitmap imgToCrop;
+    TextView phoneNumberText2;
 
     int[] rectangleCoords = new int[4];
 
+    /**
+     * This method is used to create the layout and is responsible
+     * for linking the views with logic and other methods.
+     *
+     * @param savedInstanceState the instance of the activity.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.setTitle("Pic to Phone Call");
 
+        //###### Finding the Views associated with the MainActivity layout #########
         surface = (SurfaceView)findViewById(R.id.surfaceView2);
         rectangleView = (View)findViewById(R.id.myRectangleView);
         callButton = (Button)findViewById(R.id.callButton);
         phoneNumberText = (TextView)findViewById(R.id.phoneText);
+        phoneNumberText2 = (TextView)findViewById(R.id.phoneText2);
+        //###### END finding views
 
-
+        //#### Setting the button to listen to an event and perform logic ###########
         callButton.setOnClickListener(new View.OnClickListener(){
-
+            /**
+             * This method is nested, overridden, and is being used anonymously.  There is
+             * no need to document it in JavaDoc.
+             *
+             * This method is used when the call button to get the phone number of the text
+             * that is within the rectangle. This method sends the phone number to
+             * the phone application if it senses the number to be an adequate phone number and
+             * then tells the user it is sending the number.
+             */
             @Override
             public void onClick(View v) {
 
-                //call method to call number
-                callNumber(parsePhoneNumber(phoneNumberText.getText().toString()));
+                phoneNumber = getTextFromImage(cropBitmap(imgToCrop));
+
+
+                if (checkIfPhoneNumber(phoneNumber)){
+                    callNumber(phoneNumber);
+                    phoneNumberText.setText(phoneNumberText2.getText() + " sent to phone application.");
+                }
+                else{
+                    phoneNumberText.setText("Not a possible number try again.");
+                }
 
             }
         });
+        //#### END Button setting
 
+        //#### Requesting permission to use the users camera
         if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
         {
 
@@ -90,57 +115,71 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
             surfaceHolder.addCallback(this);
             surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         }
-
+        //#### END Requesting permission
     }
 
-    public Bitmap cropBitmap(Bitmap imgToCrop){
+    /**
+     * This method does a minor check to see if the number sent to it could possibly be a phone
+     * number.  This check just makes sure that the number can not be less than 10 digits.
+     *
+     * @param phoneNum the phone number taken from the image.
+     * @return the result of the phone number test
+     */
+    public boolean checkIfPhoneNumber(String phoneNum){
+        boolean result;
 
-        /**
-         * THE CODE COMMENTED HERE IS OLD AND ONLY FOR REFERENCE
-         * THE CODE BELOW THAT SHOULD BE USED.  RESCALING THE IMAGE
-         * CAUSES ERRORS IN THE CHARACTER RECOGNITION.
-         *
-        // needed for rescaling the image.
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        int height = displayMetrics.heightPixels;
-        int width = displayMetrics.widthPixels;
+        if (phoneNum.length() >= 10){
+            result = true;
+        }
+        else{
+            result = false;
+        }
 
-        Matrix matrix = new Matrix();
-        matrix.postRotate(90); // The camera is literally 90 degress of by rotation and is neither in landscape or portrait orientation
+        return result;
+    }
 
+    /**
+     * This method is used to crop the incoming images from the surface view that
+     * is constantly looking through the camera.  This method gathers the coordinates of
+     * the rectangle view and crops the incoming images relative to the rectangle view.
+     * This method also rotates the image 90 degrees to make the image upright.
+     *
+     * @param img the image that should come from the surface view
+     * @return the cropped image and rotated image.
+     */
+    public Bitmap cropBitmap(Bitmap img){
 
-        // the rectangle in the xml has greater pixel height and width than the image
-        //
-        // Just did a test and that is wrong.  The x and y coordinates are switched.
-        //Bitmap img = Bitmap.createScaledBitmap(imgToCrop, width, height, true);
-
-         //* The createBitmap coordinates need to be played with.  x means up and down and y means left and right.
-        // * Fiddle with it until the screen is fit with the rectangle.  I did my best, but am tired now.
-
-        //img = Bitmap.createBitmap(img, rectangleCoords[0] + 100,rectangleCoords[1] - 300,  rectangleCoords[3] , rectangleCoords[2] +300 , matrix, true);
-        **/
         // Gathering coordinates from the rectangle that is with the layout.xml file.
         rectangleView.getLocationOnScreen(rectangleCoords);
         rectangleCoords[2] = rectangleView.getWidth();
         rectangleCoords[3] = rectangleView.getHeight();
 
-
-
+        // Need to rotate the image, for debug, and because the vision api works better
+        // when it is oriented normally.
         Matrix matrix = new Matrix();
         matrix.postRotate(90); // The camera is literally 90 degress of by rotation and is neither in landscape or portrait orientation
 
+         // was fiddling around with swapping the coordinates and that seems to be cleaner and more optimized.
+        img = Bitmap.createBitmap(img, rectangleCoords[1] - 250 , rectangleCoords[0] - 100,  rectangleCoords[3] - 40 , rectangleCoords[2], matrix, true);
 
-         // was fiddling around with swapping the coordinates and tDhat seems to be cleaner and more optimized.
-        imgToCrop = Bitmap.createBitmap(imgToCrop, rectangleCoords[1]- 350,rectangleCoords[0] ,  rectangleCoords[3] , rectangleCoords[2] +100 , matrix, true);
 
-        Drawable d = new BitmapDrawable(getResources(), imgToCrop);
-        callButton.setBackground(d); // just for debug and needing to see if it is cropping well.  it is ok, but needs to be better.
+        //DEBUG CODE
+//        Drawable d = new BitmapDrawable(getResources(), img);
+//        callButton.setBackground(d); // just for debug and needing to see if it is cropping well.  it is ok, but needs to be better.
+        //DEBUG CODE
 
-        return imgToCrop;
+        return img;
     }
-    public void get_text_from_image(Bitmap img){
 
+    /**
+     * This method uses Google's mobile vision API for the Optical Character Recognition (OCR).
+     * Once the image has been inferred by the API this method builds the predictions character
+     * by character until the string is complete and returns the text it found.
+     *
+     * @param img the cropped image that should have come from the cropBitmap method
+     * @return the text that was predicted
+     */
+    public String getTextFromImage(Bitmap img){
 
        TextRecognizer TR = new TextRecognizer.Builder(getApplicationContext()).build();
 
@@ -155,29 +194,49 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
        }
 
        phoneNumber = sb.toString();
-       phoneNumberText.setText(parsePhoneNumber(phoneNumber));
 
+        return phoneNumber;
     }
 
-    //method to parse string to number
+    /**
+     * This method removes the majority of characters that are not digits and returns what
+     * is left.
+     *
+     * @param pn the text that should be been predicted by using OCR
+     * @return the parse text with only characters.
+     */
     public String parsePhoneNumber(String pn){
         String phNo = pn.replaceAll("[()\\s-]+", "");
+
+        phNo = phNo.replaceAll("[^\\d.]", "");
         return phNo;
     }
 
 
-    //method to call number goes here
+    /**
+     * This method sends the phone number that has been predicted and parsed over to the phone
+     * application.
+     *
+     * @param pn the predicted and parsed phone number using OCR
+     */
     public void callNumber(String pn){
         startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", pn , null)));
     }
 
-
+    /**
+     * This method is a created from the Surface.Callback interface.
+     * This method is called once when the surface view is created.
+     * This method opens the camera, sets its parameters, and begins the
+     * camera preview display that the user will be viewing on the layout.
+     *
+     * @param holder that is used to manipulate the surface view
+     */
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         // starts the camera
         cam = Camera.open();
 
-        //Rotates the camera preview to be upright in potrait mode.
+        //Rotates the camera preview to be upright in portrait mode.
         cam.setDisplayOrientation(90);
 
         /****************************
@@ -201,7 +260,14 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
     }
 
 
-
+    /**
+     * This method is required for requesting permission to use the camera.  This method
+     * is from the application itself.
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -216,11 +282,14 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
     }
 
     /**
-     * This method is what is gathers the images from the preview and
+     * This method is overridden from the Preview.callBack interface.
+     *
+     * This method is what is gathers the images from the camera preview and
      * this is where the images are converted to bitmaps, cropped, analyzed for characters and
      * set.
-     * @param data
-     * @param camera
+     *
+     * @param data the image data from the camera
+     * @param camera the hardware camera
      */
     @Override
     public void onPreviewFrame(byte[] data, Camera camera) {
@@ -235,25 +304,27 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
 
         byte[] bytes = out.toByteArray();
         Bitmap img = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        imgToCrop = img;
 
-        // crops the bitmap, but there are qwerks.  Look at the method
+        // For a different version of the app uncomment the below
         img = cropBitmap(img);
 
-        get_text_from_image(img);
+        phoneNumber = getTextFromImage(img);
+        phoneNumberText2.setText(parsePhoneNumber(phoneNumber));
 
     }
 
-
-    /******************
-     *
-     * The methods below here aren't used but needed to be overridden by the SurfaceHolder.Callback
-     * interface.
+    /**
+     * Overridden method from SurfaceHolder.CallBack interface.  This method is unused.
      * */
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
     }
 
+    /**
+     * Overridden method from SurfaceHolder.CallBack interface.  This method is unused.
+     * */
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
 
